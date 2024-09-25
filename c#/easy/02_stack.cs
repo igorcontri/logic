939405bcdd23a6c4a6e533738ffe863c73e560bc
Igor Contri <igorcontri@gmail.com>
2024-09-25 15:57:05 -0300

    att repository

---
+++ c#/easy/02_stack.cs
@@ -0,0 +1,32 @@
+﻿char[] Insere(char letra,char[] pilha, int topo)
+{
+    pilha[topo] = letra;
+    return pilha;
+}
+
+bool EstaVazia(int topo)
+{
+    return topo == 0;
+}
+
+void Inverter(string frase)
+{
+    int qtd_caracteres = frase.Length;
+    char[] frase_invertida = new char[qtd_caracteres];
+    int topo = 0;
+
+    for (int i = 0; i <= qtd_caracteres - 1; i++)
+    {
+            Insere(frase[i], frase_invertida, topo);
+            topo += 1;
+    }
+
+    while(!EstaVazia(topo))
+    {
+        topo -= 1;
+        Console.Write(frase_invertida[topo]);
+    }
+}
+
+string frase = "Eu faco ADS";
+Inverter(frase);
