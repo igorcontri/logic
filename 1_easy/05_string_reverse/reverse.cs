ca46f89ed1a284e58bf42b080852754b621f4f4a
Igor Contri <igorcontri@gmail.com>
2024-09-26 16:09:58 -0300

    organizing repo structure

--- 1_easy/05_string_reverse/reverse.cs
+++ 1_easy/05_string_reverse/reverse.cs
@@ -12,19 +12,19 @@ bool EstaVazia(int topo)
 void Inverter(string frase)
 {
     int qtd_caracteres = frase.Length;
-    char[] frase_invertida = new char[qtd_caracteres];
+    char[] pilha = new char[qtd_caracteres];
     int topo = 0;
 
     for (int i = 0; i <= qtd_caracteres - 1; i++)
     {
-            Insere(frase[i], frase_invertida, topo);
+            Insere(frase[i], pilha, topo);
             topo += 1;
     }
 
     while(!EstaVazia(topo))
     {
         topo -= 1;
-        Console.Write(frase_invertida[topo]);
+        Console.Write(pilha[topo]);
     }
 }
 
